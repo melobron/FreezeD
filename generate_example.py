c589ce96c36f6097ffb812598689038bec7c2138
melobron <2007jiwon@gmail.com>
2022-09-19 16:01:14 +0900

    .

---
+++ generate_example.py
@@ -0,0 +1,4 @@
+
+
+# G = EasyDict(func_name='training.networks_stylegan2.G_main')       # Options for generator network.
+# D = EasyDict(func_name='training.networks_stylegan2.D_stylegan2')  # Options for discriminator network.
